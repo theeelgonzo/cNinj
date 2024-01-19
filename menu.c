1f50e117e46219319fab08e890cddd72fe351981
Jordan Godwin <jordan.godwin13@gmail.com>
2024-01-18 20:16:49 -0600

    Add switch code with character scanning.

---
+++ menu.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+    char a;
+
+    puts("Menu");
+    puts("A - Bugs");
+    puts("B - Snake Surprise");
+    puts("C - Eyeball Soup");
+    puts("Your choice: ");
+    scanf("%c", &a);
+
+    switch(a)
+    {
+        case 'A':
+        case 'a':
+            puts("Delicious! A hearty snack.");
+            break;
+        case 'B':
+        case 'b':
+            puts("A family favorite!");
+            break;
+        case 'C':
+        case 'c':
+            puts("From a secret recipe.");
+            break;
+        default:
+            puts("Invalid choice.");
+    }
+
+    return 0;
+}
