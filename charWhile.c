56a2be8d1569e720d3e00050fac13e63532c29a9
Jordan Godwin <jordan.godwin13@gmail.com>
2024-01-21 09:30:58 -0600

    Add infinite while loop with character break.

---
+++ charWhile.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int ch;
+
+    printf("Type '!' to exit: ");
+    while(1) {
+        ch = getchar();
+        if(ch == '!')
+            break;
+    }
+
+    return 0;
+}
